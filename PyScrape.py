7fb381d744dc1375b37849369cd3645d1a1dd061
Beatrice <cjselde@gmail.com>
2023-03-09 22:36:04 -0800

    Initial commit for PyScrape project

--- PyScrape.py
+++ PyScrape.py
@@ -25,12 +25,19 @@ for job_element in python_job_elements:
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
     location_element = job_element.find("p", class_="location")
+    links = job_element.find_all("a")
+#    for link in links:
+    link = links[1]
+    link_url = link["href"]
+    print(f"Apply here: {link_url}\n")
+
 #    print(job_element, end="\n" * 2)
 #    print(title_element)
     print(title_element.text.strip())  # soup object, prints string and then uses strip method on string obj
     print(company_element.text.strip())
     print(location_element.text.strip())
     print()
+
 
 
 #  python_jobs = results.find_all("h2", string="Python")
